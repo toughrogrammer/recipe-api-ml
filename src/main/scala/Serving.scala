94fa3400c3dcc1b9f2297be278bce006a855034a
변동남 <byeondongnam@byeondongnam-ui-MacBook-Pro.local>
2015-08-27 16:31:56 +0900

    add standardization

--- src/main/scala/Serving.scala
+++ src/main/scala/Serving.scala
@@ -17,9 +17,35 @@ class Serving
 
     @transient lazy val logger = Logger[this.type]
 
+    val standard: Seq[Array[ItemScore]] = if (query.limit - query.skip == 1) {
+      // if query 1 item, don't standardize
+      predictedResults.map(_.itemScores)
+    } else {
+      // Standardize the score before combine
+      val mvList: Seq[MeanAndVariance] = predictedResults.map { pr =>
+        meanAndVariance(pr.itemScores.map(_.score))
+      }
+
+      predictedResults.zipWithIndex
+        .map { case (pr, i) =>
+          pr.itemScores.map { is =>
+            // standardize score (z-score)
+            // if standard deviation is 0 (when all items have the same score,
+            // meaning all items are ranked equally), return 0.
+            val score = if (mvList(i).stdDev == 0) {
+              0
+            } else {
+              (is.score - mvList(i).mean) / mvList(i).stdDev
+            }
+
+            ItemScore(is.item, score)
+          }
+        }
+    }
+
     // 같은 item에 대해 Collaborative Filtering Recommender의 결과값과 
     // Content Based Filtering Recommender의 결과값을 합산, 정렬함
-    val combined = predictedResults.map(_.itemScores).flatten // ItemScore 배열
+    val combined = standard.flatten // ItemScore 배열
       .groupBy(_.item) // 같은 item id끼리 묶음
       .mapValues(itemScores => itemScores.map(_.score).reduce(_ + _))
       .toArray // (item id, score) 배열
